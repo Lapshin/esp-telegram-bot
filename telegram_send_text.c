3e41a0d92bc6b6b1a105a81fc73b8ab08ba84555
Alexey Lapshin <alexeyfonlapshin@gmail.com>
2019-12-24 14:48:41 +0300

    encode text before sending

--- telegram_send_text.c
+++ telegram_send_text.c
@@ -7,6 +7,7 @@
 
 
 #include <unistd.h>
+#include <ctype.h>
 #include <string.h>
 #include "esp_http_client.h"
 #include "esp_log.h"
@@ -15,12 +16,39 @@
 
 #include "telegram_send_text.h"
 
+static char rfc3986[256] = {0};
+
+static void init_rfc3986(void)
+{
+    int i;
+    for (i = 0; i < 256; i++) {
+        rfc3986[i] = isalnum(i) ||
+                i == '~' ||
+                i == '-' ||
+                i == '.' ||
+                i == '_' ? i : 0;
+    }
+}
+
+static void encode(const unsigned char *s, char *enc)
+{
+    if (rfc3986['-'] == 0)
+    {
+        init_rfc3986();
+    }
+    for (; *s != 0; s++) {
+        enc += (rfc3986[*s]) ? sprintf(enc, "%c", rfc3986[*s]) : sprintf(enc, "%%%02X", *s);
+    }
+}
+
 void telegram_send_text(const int chat_id, const char *text, const int reply_to_message_id)
 {
     char *answer = NULL;
+    char *encoded_text = calloc(strlen(text) * 3 + 1, 1);
+    encode((unsigned char *)text, encoded_text);
     char *telegram_photo_url = calloc(
                                    strlen(TELEGRAM_API_URL) +
-                                   strlen(text) +
+                                   strlen(encoded_text) +
                                    TELEGRAM_BOT_TOKEN_SIZE +
                                    strlen(TELEGRAM_METHOD_SEND_MESSAGE) +
                                    128, 1);
@@ -29,11 +57,12 @@ void telegram_send_text(const int chat_id, const char *text, const int reply_to_
             telegram_get_bot_token(),
             TELEGRAM_METHOD_SEND_MESSAGE,
             chat_id,
-            text,
+            encoded_text,
             reply_to_message_id);
 
     telegram_send_response(telegram_photo_url, &answer);
     ESP_LOGI(__FUNCTION__, "response %s", answer);
     free(answer);
+    free(encoded_text);
     answer = NULL;
 }
